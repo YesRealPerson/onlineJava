a3e4f46414253890c465c8c085d87dd13f70c503
YesRealPerson <Stephen-park@outlook.com>
2024-01-09 21:07:27 -0800

    yay

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const shell = require("shelljs");
 const fs = require('fs')
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -13,18 +14,12 @@ app.use(express.static('./static'));
 const base = "./Users/"
 const runProgram = (filename) => {
     return new Promise((res, rej) => {
-        filename = base + filename;
-        shell.exec(`javac ${filename}.java`, async (code, stdout, stderr) => {
+        let temp = `cd "${path.join(__dirname, `/Users/${filename.split("/")[0]}/`)}" & javac ${filename.split("/")[1]}.java & java ${filename.split("/")[1]}`
+        shell.exec(temp, async (code, stdout, stderr) => {
             if (code) {
                 res(stderr);
             } else {
-                shell.exec(`java -classpath ./Users/${filename.split("/")[2]} ${filename.split("/")[3]}`, async (code, stdout, stderr) => {
-                    if (code) {
-                        res(stderr);
-                    } else {
-                        res(stdout);
-                    }
-                });
+                res(stdout);
             }
         });
     })
@@ -142,11 +137,40 @@ app.get("/readfile", (req, res) => {
                 let users = JSON.parse("" + haha);
                 let user = users[req.headers.authorization].split("@")[0];
                 fs.readFile(`Users/${user}/${filename}.java`, (err, result) => {
-                    if(err){
-                        res.status(500).send(err);
+                    if (err) {
+                        fs.writeFile(`Users/${user}/${filename}.java`, [`class ${filename} {`, '\tpublic static void main(String[] args){', '\t\tSystem.out.println("Hello world!");', '\t}', '}'].join('\n'), (err) => {
+                            if (err) {
+                                res.status(500).send(err);
+                            } else {
+                                res.status(200).send([`class ${filename} {`, '\tpublic static void main(String[] args){', '\t\tSystem.out.println("Hello world!");', '\t}', '}'].join('\n'))
+                            }
+                        })
                     }
-                    else{
-                        res.status(200).send(""+result);
+                    else {
+                        res.status(200).send("" + result);
+                    }
+                })
+            }
+        })
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+app.post("/makefile", (req, res) => {
+    try {
+        let filename = req.query.name;
+        fs.readFile("users.json", (err, haha) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                let users = JSON.parse("" + haha);
+                let user = users[req.headers.authorization].split("@")[0];
+                fs.writeFile(`Users/${user}/${filename}.java`, [`class ${filename} {`, '\tpublic static void main(String[] args){', '\t\tSystem.out.println("Hello world!");', '\t}', '}'].join('\n'), (err) => {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.sendStatus(200)
                     }
                 })
             }
@@ -168,12 +192,12 @@ app.get("/filelist", (req, res) => {
                     result = "";
                     files.forEach(file => {
                         let temp = file.split(".");
-                      if(temp[temp.length-1] == "java"){
-                        result += file+",";
-                      }
+                        if (temp[temp.length - 1] == "java") {
+                            result += file + ",";
+                        }
                     });
-                    res.status(200).send(result);
-                  });
+                    res.status(200).send(result.substring(0, result.length - 1));
+                });
             }
         })
     } catch (err) {
--- static/communicate.js
+++ static/communicate.js
@@ -3,20 +3,21 @@ const sendRunRequest = async () => {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
-            "Authorization": document.cookie.split("=")[1]
+            "Authorization": document.cookie.split("key=")[1]
         },
         body: editor.getValue()
     })
     document.getElementById("console").innerText= await response.text()
+    readFiles();
 }
 
 const getFile = async (filename) => {
     const response = await fetch("./readfile?name="+filename, {
         method: 'GET',
         headers: {
-            "Authorization": document.cookie.split("=")[1]
+            "Authorization": document.cookie.split("key=")[1]
         },
-    })
+    });
     editor.getModel().setValue(await response.text());
 }
 
@@ -24,7 +25,23 @@ const readFiles = async () => {
     const response = await fetch("./filelist", {
         method: 'GET',
         headers: {
-            "Authorization": document.cookie.split("=")[1]
+            "Authorization": document.cookie.split("key=")[1]
         },
     })
+    let files = await response.text();
+    files = files.split(",");
+    let list = document.getElementById("list");
+    list.innerText="";
+    for(i in files){
+        let temp = document.createElement("a");
+        temp.innerText = files[i];
+        temp.setAttribute("onclick", `getFile("${files[i].split(".")[0]}")`);
+        list.appendChild(temp);
+        temp = document.createElement("br");
+        list.appendChild(temp);
+    }
+}
+
+const makeFile = async () => {
+    prompt("Enter file name")
 }
